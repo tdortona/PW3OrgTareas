4a2f2174b73c6842f8035adcf06b32a13e35870f
Tomas <tomasdo@lagash.com>
2018-07-01 19:56:44 -0300

    Se agregan filtros para tareas completadas y no completadas

--- PW3OrgTareas/PW3OrgTareas/Scripts/Tarea/Index.js
+++ PW3OrgTareas/PW3OrgTareas/Scripts/Tarea/Index.js
@@ -10,3 +10,33 @@
         }
     });
 }
+
+function ocultarCompletadas(element) {
+    var rows = $("#misTareas").find("tr");
+
+    $.each(rows, function (index, row) {
+        if ($(row).data("completada").toLowerCase() === "true") {
+            if ($(element).prop("checked") == true) {
+                $(row).hide();
+            }
+            else {
+                $(row).show();
+            }
+        }
+    });
+}
+
+function ocultarNoCompletadas(element) {
+    var rows = $("#misTareas").find("tr");
+
+    $.each(rows, function (index, row) {
+        if ($(row).data("completada").toLowerCase() === "false") {
+            if ($(element).prop("checked") == true) {
+                $(row).hide();
+            }
+            else {
+                $(row).show();
+            }
+        }
+    });
+}
